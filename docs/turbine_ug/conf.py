9fcea87b697b0ea4571c6bd37e23162e46f8413b
Pavan Dharmavaram <pavankumar.giridhar@swimlane.com>
2024-07-10 12:20:34 +0530

    pdf html builders

--- docs/turbine_ug/conf.py
+++ docs/turbine_ug/conf.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath('.'))
 
 # Project information
@@ -15,7 +16,8 @@ release = '24.2'
 
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosectionlabel',   
+    'sphinx.ext.autosectionlabel',
+    'sphinxcontrib.pdfbuilder',
 ]
 
 
